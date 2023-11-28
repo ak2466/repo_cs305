4d79f14bb5905d2cad9c241d8b969a5dcbe30cb5
ak2466 <149115119+ak2466@users.noreply.github.com>
2023-11-27 22:44:18 -0700

    Create README.sh

---
+++ README.sh
@@ -0,0 +1,20 @@
+# repo_cs305
+
+## To do: 
+
+- [x] place yoghurt in blender
+
+- [ ] place frozen fruit in blender
+
+- [ ] add juice to blender
+
+## Some time in the future:
+- [ ] Find owners' manual of blender
+
+- [ ] Use blender to blend smoothie
+
+
+
+## License for Project: The GNU’s General Public License 
+
+This liscense has the greatest popularity of any of the open source licenses. As a copyleft license, if there is any software that uses any GPL component as a base must be open source. 
